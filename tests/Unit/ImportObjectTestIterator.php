6310a40238ba6fe5d68b32eb7646260b4cd1a33d
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-03-07 10:42:43 -0300

    Configuração do phpunit para enxergar as Libs dos testes do pacote

--- tests/Unit/ImportObjectTestIterator.php
+++ tests/Unit/ImportObjectTestIterator.php
@@ -2,7 +2,7 @@
 
 namespace ReportCollection\Tests\Unit;
 
-class ImportObjectTestIterator implements Iterator {
+class ImportObjectTestIterator implements \Iterator {
 
     private $position = 0;
 
@@ -23,31 +23,26 @@ class ImportObjectTestIterator implements Iterator {
 
     public function rewind() 
     {
-        var_dump(__METHOD__);
         $this->position = 0;
     }
 
     public function current() 
     {
-        var_dump(__METHOD__);
         return $this->array[$this->position];
     }
 
     public function key() 
     {
-        var_dump(__METHOD__);
         return $this->position;
     }
 
     public function next() 
     {
-        var_dump(__METHOD__);
         ++$this->position;
     }
 
     public function valid() 
     {
-        var_dump(__METHOD__);
         return isset($this->array[$this->position]);
     }
 }
